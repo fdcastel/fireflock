fed774e54a7c773e4e070ae0cff9f0e1dbea11ce
F.D.Castel <fdcastel@gmail.com>
2024-01-02 11:36:45 -0300

    Fix for Firebird 2.5.

--- rebuild-databases.sh
+++ rebuild-databases.sh
@@ -13,9 +13,9 @@ while [[ $# -gt 0 ]]; do
     -t|--target) OPT_TARGET="$2" ; shift ; shift ;;
 
     -p|--prefix) OPT_PREFIX="$2" ; shift ; shift ;;
-    
+
     --pagesize) OPT_PAGESIZE="$2" ; shift ; shift ;;
-    
+
     --charset) OPT_CHARSET="$2" ; shift ; shift ;;
 
     -h|--help)
@@ -30,16 +30,29 @@ while [[ $# -gt 0 ]]; do
   esac
 done
 
-declare -a ENGINES=("FB50" "FB40" "FB30")
+declare -a ENGINES=("FB50" "FB40" "FB30" "FB25")
 for eng in ${ENGINES[@]}
 do
     OPT_DATABASE="$OPT_TARGET/$OPT_PREFIX.$eng.fdb"
     rm -f $OPT_DATABASE
 
     BIN_PATH_ENV_NAME="${eng}_BIN_PATH"
-    cat <<EOF | ${!BIN_PATH_ENV_NAME}/isql -quiet 2> /dev/null
+    BIN_PATH=${!BIN_PATH_ENV_NAME}
+
+    if [ "$eng" == "FB25" ]; then
+        # Patch for Firebird 2.5 -- https://groups.google.com/g/firebird-support/c/T6Nu6snaBWM/m/yLYqcJj0BAAJ
+        export LD_LIBRARY_PATH=$BIN_PATH/../lib
+        export FIREBIRD=$BIN_PATH/..
+    fi
+
+    cat <<EOF | $BIN_PATH/isql -quiet 2> /dev/null
         CREATE DATABASE '$OPT_DATABASE'
             USER 'SYSDBA' PASSWORD 'masterkey'
             PAGE_SIZE $OPT_PAGESIZE DEFAULT CHARACTER SET $OPT_CHARSET;
 EOF
+
+    if [ "$eng" == "FB25" ]; then
+        export LD_LIBRARY_PATH=
+        export FIREBIRD=
+    fi
 done
